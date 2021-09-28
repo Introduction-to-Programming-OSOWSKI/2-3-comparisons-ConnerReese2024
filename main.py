12b862144b528fa55f6fd7c7b51b3e6ab3525fd7
ConnerReese2024 <820449@mystma.org>
2021-09-28 09:33:45 -0500

    3

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-#WRITE YOUR CODE IN THIS FILE
+def greaterThan(x, y):
+    if x > y:
+        return True
+    else: 
+        return False
+
+
+def lessThan(x, y):
+    if x < y:
+        return True
+    else:
+        return False
+
+
+def equalTo(x, y):
+    if x == y:
+        return True
+    else:
+        return False
+
+
+def greaterOREqualTo(x, y):
+    if x >= y:
+        return True
+    else:
+        return False
+
+
+def lessOrEqualTo(x, y):
+    if x <= y:
+        return True
+    else:
+        return False
